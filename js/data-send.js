9390e64c1340dab96fc0a87c9c009f1cf1a04f38
naokigt <naokigztt0@gmail.com>
2016-01-12 10:38:44 +0000

    画像ファイルの追加、ajaxの追加

---
+++ js/data-send.js
@@ -0,0 +1,23 @@
+$(function(){
+	var sendData = {
+		gender : $.cookie('sex'),
+		name : $.cookie('userName'),
+		posision : $.cookie('posision'),
+		costume_head : $.cookie('costumeHead'),
+		costume_up : $.cookie('costumeUp'),
+		costume_under : $.cookie('costumeUnder'),
+		maker : $.cookie('selectedMaker'),
+		review : $.cookie('review'),
+		finish_time : $.cookie('finishTime')
+	}
+	
+	$.ajax({
+		type: "GET",
+		url: "http://localhost/cosplay-tailors-backend/public/api/add_makes.json",
+		dataType: 'json',
+		data: sendData
+	})
+	.done(function(msg){
+		console.log(msg);
+	});
+});
